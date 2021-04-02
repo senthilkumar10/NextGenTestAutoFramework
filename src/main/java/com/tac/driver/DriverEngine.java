a857dfbf5c76dd391e85726bf1d407637b5aa800
senthil <senthilkumar10@gmail.com>
2021-04-02 23:26:26 +0100

    Modified code for Docker exec

--- src/main/java/com/tac/driver/DriverEngine.java
+++ src/main/java/com/tac/driver/DriverEngine.java
@@ -1,33 +1,32 @@
 package com.tac.driver;
 
+import com.tac.customexceptions.FrameworkExceptions;
+import com.tac.factories.DriverFactory;
 import com.tac.utility.ReadPropertyFile;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Objects;
 
 public final class DriverEngine {
 
-    private DriverEngine() {
-    }
+    private DriverEngine() {}
 
     public static void initDriver(String browser){
-        if(browser.equalsIgnoreCase("chrome")){
-            if (Objects.isNull(DriverManager.getDriver())) {
-                WebDriverManager.chromedriver().setup();
-                DriverManager.setDriver(new ChromeDriver());
-
+        if(Objects.isNull(DriverManager.getDriver())){
+            try {
+                DriverManager.setDriver(DriverFactory.getDriver(browser));
+            } catch (MalformedURLException e) {
+                throw new FrameworkExceptions("Browser Innovocation Failed Exceptions");
             }
-        }else if(browser.equalsIgnoreCase("firefox")){
-            if (Objects.isNull(DriverManager.getDriver())) {
-                WebDriverManager.firefoxdriver().setup();
-                DriverManager.setDriver(new FirefoxDriver());
-            }
+            DriverManager.getDriver().get(ReadPropertyFile.getValue("url"));
         }
-
-        DriverManager.getDriver().get(ReadPropertyFile.getValue("url"));
-
     }
 
     public static void quitDriver() {
---
+++ src/main/java/com/tac/factories/DriverFactory.java
@@ -0,0 +1,50 @@
+package com.tac.factories;
+
+import com.tac.driver.DriverManager;
+import com.tac.utility.ReadPropertyFile;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author senthil
+ */
+public final class DriverFactory {
+
+    private DriverFactory() {
+    }
+
+    public static WebDriver getDriver(String browser) throws MalformedURLException {
+        WebDriver driver = null;
+        String runmode = ReadPropertyFile.getValue("runmode");
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            if (runmode.equalsIgnoreCase("remote")) {
+                DesiredCapabilities cap = new DesiredCapabilities();
+                cap.setBrowserName(BrowserType.CHROME);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+            } else {
+                DriverManager.setDriver(new ChromeDriver());
+                driver = new ChromeDriver();
+            }
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            if (runmode.equalsIgnoreCase("remote")) {
+                DesiredCapabilities cap = new DesiredCapabilities();
+                cap.setBrowserName(BrowserType.FIREFOX);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+            } else {
+                DriverManager.setDriver(new FirefoxDriver());
+                driver = new FirefoxDriver();
+            }
+        }
+        return driver;
+    }
+
+}
